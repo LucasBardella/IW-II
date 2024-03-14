68c3620d47475f8267b10cda3c91736f9b8d059d
LucasBardella <lucasss.bardella.f@gmail.com>
2024-03-14 09:22:29 -0300

    exercicios

--- exercicios basicos/5/js/script.js
+++ exercicios basicos/5/js/script.js
@@ -1,7 +1,6 @@
-var preco = document.getElementById('valor')
-
 function botao() {
     var botao = prompt('Digite o que deseja comprar: ')
+    produto.innerHTML = botao
     var preco = prompt('Digite o valor do produto:')
     valor.innerHTML = preco
 }
