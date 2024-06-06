817caacad8663efe1f184afbe1709b64d92de07a
LucasBardella <lucasss.bardella.f@gmail.com>
2024-06-06 09:06:24 -0300

    06/06

--- curso_node/04_Html/index.js
+++ curso_node/04_Html/index.js
@@ -1,24 +1,24 @@
 const express = require("express")
 const app = express()
 
-app.get('/',function(req,res){
-    res.send('seja bem vindo baitola!')
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/paginas/index.html')
 })
 
-app.get('/blog',function(req,res){
-    res.send('Essa é página do Blog')
+app.get('/sobre', function (req, res) {
+    res.sendFile(__dirname + '/paginas/sobre.html')
 })
 
-app.get('/sobre',function(req,res){
-    res.send('Página sobre')
+app.get('/blog', function (req, res) {
+    res.send('Página blog')
 })
 
-app.get('/cadastro/:nome/:idade',function(req,res){
+app.get('/cadastro/:nome/:idade', function (req, res) {
     res.send('Olá seu nome: ' + req.params.nome + '\nSua idade: ' + req.params.idade)
-    //res.send(req.params)
+
 })
 
 
-app.listen(8081, function (){
+app.listen(8081, function () {
     console.log('Servidor girando e dançando em http://localhost:8081')
 })
