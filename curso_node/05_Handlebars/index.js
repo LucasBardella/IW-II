a363f5ab690c476a0e0b2bd68074e1a1f5829e5f
LucasBardella <lucasss.bardella.f@gmail.com>
2024-06-13 09:01:03 -0300

    ...

--- curso_node/05_Handlebars/index.js
+++ curso_node/05_Handlebars/index.js
@@ -17,10 +17,26 @@ const sequelize = new Sequelize('test', 'root', '', {
 
 //Rotas
 app.get('/cadastro', function (req, res) {
-    res.send('ROTA DE CADASTRO DE POST')
+    // res.send('ROTA DE CADASTRO DE POST')
+    res.render('formulario')
+})
+
+app.post('/add', function (req, res) {
+    res.send('FORMULARIO RECEBIDO')
+    //res.render('formulario')
 })
 
 
+
+
+
+
+
+
+
+
+
+//Chamar a hospedagem
 app.listen(8081, function () {
     console.log('Servidor girando e dançando em http://localhost:8081')
 })
